75b1ef8527ffb94f41117f69223efce0ef79d39d
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-05-13 20:33:34 +0900

    add map view sample

---
+++ scripts/mapview/main.py
@@ -0,0 +1,9 @@
+from kivy.garden.mapview import MapView
+from kivy.app import App
+
+class MapViewApp(App):
+    def build(self):
+        mapview = MapView(zoom=15, lat=35.681382, lon=139.766084)
+        return mapview
+
+MapViewApp().run()
